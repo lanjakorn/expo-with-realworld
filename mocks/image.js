ba70919f48b870a558eee730ef94dfed916fc49f
nattatorn.dev <nattatorn.dev@gmail.com>
2017-08-02 21:21:45 +0700

    ✨ add mock image service

---
+++ mocks/image.js
@@ -0,0 +1,7 @@
+const categories = data => ( {
+  ...data,
+  image:
+    'http://www.ricoh.co.th/sites/www.ricoh.co.th/files/styles/product_image_large/public/MP_C307_m3.jpg?itok=NlrAveR1',
+} )
+
+export { categories }
