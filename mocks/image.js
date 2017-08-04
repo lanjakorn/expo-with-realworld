8857562539b13b509eb62b74b88e91fa5d9475cb
nattatorn.dev <nattatorn.dev@gmail.com>
2017-08-04 22:19:08 +0700

    📝 update mock image service

--- mocks/image.js
+++ mocks/image.js
@@ -27,6 +27,31 @@ const subChildCategories = data =>
 const products = data => {
   return {
     ...data,
+    description:
+      'With nine models available, these hard workers form part of our flagship A3 colour Smart MFP range.',
+    title:
+      'Much more than a new range. It’s a whole new way to connect your business.',
+    pros: [
+      'Intuitive touch and swipe Smart Operation Panel',
+      'New Intel Processor increases productivity',
+      'Human Detection Sensor',
+      'Offers a range of great finishing options',
+      'Professional output with banner print capability',
+    ],
+    tags: [
+      'http://www.ricoh.co.th/sites/www.ricoh.co.th/files/icon_func_new_0.gif',
+      'http://www.ricoh.co.th/sites/www.ricoh.co.th/files/icon_func_copy.gif',
+      'http://www.ricoh.co.th/sites/www.ricoh.co.th/files/icon_func_print.gif',
+      'http://www.ricoh.co.th/sites/www.ricoh.co.th/files/icon_func_scan_0.gif',
+      'http://www.ricoh.co.th/sites/www.ricoh.co.th/files/icon_func_fax.gif',
+
+      'http://www.ricoh.co.th/sites/www.ricoh.co.th/files/icon_func_A3.gif',
+      'http://www.ricoh.co.th/sites/www.ricoh.co.th/files/icon_func_A4.gif',
+
+      'http://www.ricoh.co.th/sites/www.ricoh.co.th/files/icon_spec_lan.gif',
+      'http://www.ricoh.co.th/sites/www.ricoh.co.th/files/icon_spec_usb_0.gif',
+      'http://www.ricoh.co.th/sites/www.ricoh.co.th/files/icon_spec_wifi.gif',
+    ],
     urls: {
       imgs: [
         'http://www.ricoh.co.th/sites/www.ricoh.co.th/files/SPC240DN_OK.jpg',
