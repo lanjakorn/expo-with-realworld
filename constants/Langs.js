b23abbcbb7be546724d91669d4573c830ffe29ed
nattatorn.dev <nattatorn.dev@gmail.com>
2017-08-07 22:32:28 +0700

    ➕ add langs constant

---
+++ constants/Langs.js
@@ -0,0 +1,12 @@
+export default ( langs = {
+  en: {
+    productPros: 'Advantages',
+    price: 'price',
+    baht: 'Baht',
+  },
+  th: {
+    productPros: 'ข้อดีของผลิตภัณฑ์',
+    price: 'ราคา',
+    baht: 'บาท',
+  },
+} )
--- constants/index.js
+++ constants/index.js
@@ -2,5 +2,6 @@ import Alerts from './Alerts'
 import Colors from './Colors'
 import DefaultTab from './DefaultTab'
 import Layout from './Layout'
+import Langs from './Langs'
 
-export { Alerts, Colors, DefaultTab, Layout }
+export { Alerts, Colors, DefaultTab, Layout, Langs }
