9f1fb26a4f8f1e2f48f30b64e68db4134cea08de
nattatorn.dev <nattatorn.dev@gmail.com>
2017-08-12 20:05:41 +0700

    ➕ add mock house data

---
+++ mocks/house.js
@@ -0,0 +1,29 @@
+const houseMenu = [
+  {
+    title: 'Higher Education',
+    description:
+      'Ut ullam dolorem earum nulla. Eligendi voluptas occaecati cupiditate qui.',
+  },
+  {
+    title: 'Healthcare',
+    description:
+      'Ut ullam dolorem earum nulla. Eligendi voluptas occaecati cupiditate qui.',
+  },
+  {
+    title: 'Hotel',
+    description:
+      'Ut ullam dolorem earum nulla. Eligendi voluptas occaecati cupiditate qui.',
+  },
+  {
+    title: 'Logistic',
+    description:
+      'Ut ullam dolorem earum nulla. Eligendi voluptas occaecati cupiditate qui.',
+  },
+  {
+    title: 'Bank',
+    description:
+      'Ut ullam dolorem earum nulla. Eligendi voluptas occaecati cupiditate qui.',
+  },
+]
+
+export { houseMenu }
--- mocks/product.js
+++ mocks/product.js
@@ -36,6 +36,29 @@ const products = data => {
         value: 120,
       },
     },
+    faqs: [
+      {
+        title:
+          'Et perspiciatis dolor perferendis laboriosam enim Et perspiciatis dolor perferendis laboriosam enim.',
+        content:
+          'Quam omnis sapiente autem sit quia quod consequatur. Est quo est. At in sunt placeat repudiandae sint aperiam ut rerum est. Quidem quia necessitatibus provident omnis commodi rerum omnis voluptatem. Veniam in ut non quo qui minima. Magnam qui aut incidunt. Debitis molestias esse. Dolore laudantium qui. Hic rem consequatur molestiae. Corrupti officia explicabo eligendi eum. Et dolorem ut natus laborum deleniti est. Id ut neque fugit et qui qui minus. Neque vel et ex beatae aut libero rerum deserunt omnis. Odio fugit numquam quod voluptates asperiores aut quasi rerum.',
+      },
+      {
+        title: 'Et perspiciatis dolor perferendis laboriosam enim.',
+        content:
+          'Quam omnis sapiente autem sit quia quod consequatur. Est quo est. At in sunt placeat repudiandae sint aperiam ut rerum est. Quidem quia necessitatibus provident omnis commodi rerum omnis voluptatem. Veniam in ut non quo qui minima. Magnam qui aut incidunt. Debitis molestias esse. Dolore laudantium qui. Hic rem consequatur molestiae. Corrupti officia explicabo eligendi eum. Et dolorem ut natus laborum deleniti est. Id ut neque fugit et qui qui minus. Neque vel et ex beatae aut libero rerum deserunt omnis. Odio fugit numquam quod voluptates asperiores aut quasi rerum.',
+      },
+      {
+        title: 'Et perspiciatis dolor perferendis laboriosam enim.',
+        content:
+          'Quam omnis sapiente autem sit quia quod consequatur. Est quo est. At in sunt placeat repudiandae sint aperiam ut rerum est. Quidem quia necessitatibus provident omnis commodi rerum omnis voluptatem. Veniam in ut non quo qui minima. Magnam qui aut incidunt. Debitis molestias esse. Dolore laudantium qui. Hic rem consequatur molestiae. Corrupti officia explicabo eligendi eum. Et dolorem ut natus laborum deleniti est. Id ut neque fugit et qui qui minus. Neque vel et ex beatae aut libero rerum deserunt omnis. Odio fugit numquam quod voluptates asperiores aut quasi rerum.',
+      },
+      {
+        title: 'Et perspiciatis dolor perferendis laboriosam enim.',
+        content:
+          'Quam omnis sapiente autem sit quia quod consequatur. Est quo est. At in sunt placeat repudiandae sint aperiam ut rerum est. Quidem quia necessitatibus provident omnis commodi rerum omnis voluptatem. Veniam in ut non quo qui minima. Magnam qui aut incidunt. Debitis molestias esse. Dolore laudantium qui. Hic rem consequatur molestiae. Corrupti officia explicabo eligendi eum. Et dolorem ut natus laborum deleniti est. Id ut neque fugit et qui qui minus. Neque vel et ex beatae aut libero rerum deserunt omnis. Odio fugit numquam quod voluptates asperiores aut quasi rerum.',
+      },
+    ],
     reviewScore: 12,
     reviewCount: 3,
     urls: {
