e4449dcd303def53f996c2ee52447a7c3df61dba
nattatorn.dev <nattatorn.dev@gmail.com>
2017-08-13 22:35:46 +0700

    📝 update rootReducer, rootSaga

--- rootReducer.js
+++ rootReducer.js
@@ -1,14 +1,18 @@
 import { combineReducers } from 'redux'
 import { reducers as categoriesReducer } from 'modules/Categories'
+import { reducers as faqsReducer } from 'modules/Faqs'
+import { reducers as houseCategoriesReducer } from 'modules/HouseCategories'
 import { reducers as productsReducer } from 'modules/Products'
 import { reducers as searchReducers } from 'modules/Search'
 import { reducers as settingsReducer } from 'modules/Settings'
 
 const rootReducer = combineReducers( {
   ...categoriesReducer,
+  ...faqsReducer,
+  ...houseCategoriesReducer,
+  ...productsReducer,
   ...searchReducers,
   ...settingsReducer,
-  ...productsReducer,
 } )
 
 export default rootReducer
--- rootSaga.js
+++ rootSaga.js
@@ -1,5 +1,7 @@
 import { all } from 'redux-saga/effects'
 import { sagas as categoriesSagas } from 'modules/Categories'
+import { sagas as faqsSagas } from 'modules/Faqs'
+import { sagas as houseCategoriesSagas } from 'modules/HouseCategories'
 import { sagas as productsSagas } from 'modules/Products'
 import { sagas as searchSagas } from 'modules/Search'
 import { sagas as settingsSagas } from 'modules/Settings'
@@ -7,6 +9,8 @@ import { sagas as settingsSagas } from 'modules/Settings'
 export default function* rootSagas() {
   yield all( [
     ...categoriesSagas,
+    ...faqsSagas,
+    ...houseCategoriesSagas,
     ...productsSagas,
     ...searchSagas,
     ...settingsSagas,
