60f7730d5e66775d116434654668c03e184ade42
nattatorn.dev <nattatorn.dev@gmail.com>
2017-08-21 16:53:44 +0700

    📝	update object utilities

--- utilities/object.js
+++ utilities/object.js
@@ -2,4 +2,8 @@ const getFirstByKey = ( { item, key } ) => {
   return item[ key ].slice( 0, 1 )[ 0 ]
 }
 
-export { getFirstByKey }
+const getLastByKey = ( { item, key } ) => {
+  return item[ key ].slice( 0, item[ key ].length )[ item[ key ].length - 1 ]
+}
+
+export { getFirstByKey, getLastByKey }
