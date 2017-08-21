a86b84a4c32fbfb84cb3aafc6823005f5afdf3a2
nattatorn.dev <nattatorn.dev@gmail.com>
2017-08-21 17:03:43 +0700

    ➕ add mores in mocks

---
+++ mocks/mores.js
@@ -0,0 +1,34 @@
+export default {
+  promotions: {
+    title: 'Promotion / Hot Seals',
+    navigate: 'promotions',
+  },
+  companyProfiles: {
+    title: 'Company Profile',
+    navigate: 'companyProfiles',
+  },
+  caseStudies: {
+    title: 'Case Study',
+    navigate: 'caseStudies',
+  },
+  newsLetters: {
+    title: 'Newsletter',
+    navigate: 'newsLetters',
+  },
+  contactUs: {
+    title: 'Contact Us',
+    navigate: 'contactUs',
+  },
+  apps: {
+    title: 'App',
+    navigate: 'apps',
+  },
+  login: {
+    title: 'Sales Log In',
+    navigate: 'login',
+  },
+  touchs: {
+    title: 'RICOH Touch',
+    navigate: 'touchs',
+  },
+}
