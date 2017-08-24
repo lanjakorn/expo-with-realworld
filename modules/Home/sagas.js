423eddbe057e0bf0b29e96de916d56e94fbc6946
nattatorn.dev <nattatorn.dev@gmail.com>
2017-08-24 22:44:21 +0700

    ⚡️ update sequential prefetching Algorithm on home saga

--- modules/Home/sagas.js
+++ modules/Home/sagas.js
@@ -56,78 +56,113 @@ const getFireBaseByRef = db => {
   } )
 }
 
+function* getCaseStudies() {
+  yield put( caseStudiesAction.caseStudies.request() )
+  const caseStudies = yield call( getFireBaseByRef, 'caseStudies' )
+  const normalizedCaseStudies = yield call(
+    caseStudiesNormalized.normalizedCaseStudies,
+    caseStudies
+  )
+  yield put( caseStudiesAction.caseStudies.success( normalizedCaseStudies ) )
+}
+
+function* getCategories() {
+  yield put( categoriesAction.categories.request() )
+  const categories = yield call( getFireBaseByRef, 'categories' )
+  const normalizedCategories = yield call(
+    categoriesNormalized.normalizedCategories,
+    categories
+  )
+  yield put( categoriesAction.categories.success( normalizedCategories ) )
+}
+
+function* getContacts() {
+  yield put( contactsAction.contacts.request() )
+  const contacts = yield call( getFireBaseByRef, 'contacts' )
+  const normalizedContacts = yield call(
+    contactsNormalized.normalizedContacts,
+    contacts
+  )
+  yield put( contactsAction.contacts.success( normalizedContacts ) )
+}
+
+function* getContactUs() {
+  yield put( contactsAction.contacts.request() )
+  const contactUs = yield call( getFireBaseByRef, 'contactUs' )
+  const normalizedContactUs = yield call(
+    contactUsNormalized.normalizedContactUs,
+    contactUs
+  )
+  yield put( contactUsAction.contactUs.success( normalizedContactUs ) )
+}
+
+function* getFaqs() {
+  yield put( faqsAction.faqs.request() )
+  const faqs = yield call( getFireBaseByRef, 'faqs' )
+  const normalizedFaqs = yield call( faqsNormalized.normalizedFaqs, faqs )
+  yield put( faqsAction.faqs.success( normalizedFaqs ) )
+}
+
+function* getHouseCategories() {
+  yield put( houseCategoriesAction.houseCategories.request() )
+  const houseCategories = yield call( getFireBaseByRef, 'houseCategories' )
+  const normalizedHouseCategories = yield call(
+    houseCategoriesNormalized.normalizedHouseCategories,
+    houseCategories
+  )
+  yield put(
+    houseCategoriesAction.houseCategories.success( normalizedHouseCategories )
+  )
+}
+
+function* getServices() {
+  yield put( servicesAction.services.request() )
+  const services = yield call( getFireBaseByRef, 'services' )
+  const normalizedServices = yield call(
+    servicesNormalized.normalizedServices,
+    services
+  )
+  yield put( servicesAction.services.success( normalizedServices ) )
+}
+
+function* getSolutionCategories() {
+  yield put( solutionCategoriesAction.solutionCategories.request() )
+  const solutionCategories = yield call( getFireBaseByRef, 'solutionCategories' )
+  const normalizedSolutionCategories = yield call(
+    solutionCategoriesNormalized.normalizedSolutionCategories,
+    solutionCategories
+  )
+  yield put(
+    solutionCategoriesAction.solutionCategories.success(
+      normalizedSolutionCategories
+    )
+  )
+}
+
+function* getSolutions() {
+  yield put( solutionsAction.solutions.request() )
+  const solutions = yield call( getFireBaseByRef, 'solutions' )
+  const normalizedSolutions = yield call(
+    solutionsNormalized.normalizedSolutions,
+    solutions
+  )
+  yield put( solutionsAction.solutions.success( normalizedSolutions ) )
+}
+
 function* watchInitHomeScreen() {
   while ( yield take( INIT_HOME_SCREEN ) ) {
     yield put( homeAction.request() )
 
-    const [
-      caseStudies,
-      categories,
-      contacts,
-      contactUs,
-      faqs,
-      houseCategories,
-      services,
-      solutionCategories,
-      solutions,
-    ] = yield all( [
-      yield call( getFireBaseByRef, 'caseStudies' ),
-      yield call( getFireBaseByRef, 'categories' ),
-      yield call( getFireBaseByRef, 'contacts' ),
-      yield call( getFireBaseByRef, 'contactUs' ),
-      yield call( getFireBaseByRef, 'faqs' ),
-      yield call( getFireBaseByRef, 'houseCategories' ),
-      yield call( getFireBaseByRef, 'services' ),
-      yield call( getFireBaseByRef, 'solutionCategories' ),
-      yield call( getFireBaseByRef, 'solutions' ),
-    ] )
-
-    const [
-      normalizedCaseStudies,
-      normalizedCategories,
-      normalizedContacts,
-      normalizedContactUs,
-      normalizedFaqs,
-      normalizedHouseCategories,
-      normalizedServices,
-      normalizedSolutionCategories,
-      normalizedSolutions,
-    ] = yield all( [
-      yield call( caseStudiesNormalized.normalizedCaseStudies, caseStudies ),
-      yield call( categoriesNormalized.normalizedCategories, categories ),
-      yield call( contactsNormalized.normalizedContacts, contacts ),
-      yield call( contactUsNormalized.normalizedContactUs, contactUs ),
-      yield call( faqsNormalized.normalizedFaqs, faqs ),
-      
-      yield call(
-        houseCategoriesNormalized.normalizedHouseCategories,
-        houseCategories
-      ),
-      yield call( servicesNormalized.normalizedServices, services ),
-      yield call(
-        solutionCategoriesNormalized.normalizedSolutionCategories,
-        solutionCategories
-      ),
-
-      yield call( solutionsNormalized.normalizedSolutions, solutions ),
-    ] )
-
     yield all( [
-      yield put( caseStudiesAction.caseStudies.success( normalizedCaseStudies ) ),
-      yield put( categoriesAction.categories.success( normalizedCategories ) ),
-      yield put( contactsAction.contacts.success( normalizedContacts ) ),
-      yield put( contactUsAction.contactUs.success( normalizedContactUs ) ),
-      yield put( faqsAction.faqs.success( normalizedFaqs ) ),
-      yield put(
-        houseCategoriesAction.houseCategories.success( normalizedHouseCategories )
-      ),
-      yield put( servicesAction.services.success( normalizedServices ) ),
-      yield put(
-        solutionCategoriesAction.solutionCategories.success(
-          normalizedSolutionCategories
-        )
-      ),
-      yield put( solutionsAction.solutions.success( normalizedSolutions ) ),
+      yield fork( getCaseStudies ),
+      yield fork( getCategories ),
+      yield fork( getContacts ),
+      yield fork( getContactUs ),
+      yield fork( getFaqs ),
+      yield fork( getHouseCategories ),
+      yield fork( getServices ),
+      yield fork( getSolutionCategories ),
+      yield fork( getSolutions ),
     ] )
 
     yield put( homeAction.success() )
