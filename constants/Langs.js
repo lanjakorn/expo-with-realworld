dc62354b8a0f28f561182dd412eda3a0b83eb30f
nattatorn.dev <nattatorn.dev@gmail.com>
2017-08-25 21:39:42 +0700

    📝	update colors, lang constants

--- constants/Langs.js
+++ constants/Langs.js
@@ -3,10 +3,14 @@ export default {
     productPros: 'Advantages',
     price: 'price',
     baht: 'Baht',
+    minPrice: 'Minimum Price',
+    maxPrice: 'Maximum Price',
   },
   th: {
     productPros: 'ข้อดีของผลิตภัณฑ์',
     price: 'ราคา',
     baht: 'บาท',
+    minPrice: 'ราคาต่ำสุด',
+    maxPrice: 'ราคาสูงสุด',
   },
 }
