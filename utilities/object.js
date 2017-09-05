22939ac7060af41e01c9a12bf84afb68edd24988
nattatorn.dev <nattatorn.dev@gmail.com>
2017-09-05 22:09:38 +0700

    📝 update object utilities

--- utilities/object.js
+++ utilities/object.js
@@ -6,4 +6,19 @@ const getLastByKey = ( { item, key } ) => {
   return item[ key ].slice( 0, item[ key ].length )[ item[ key ].length - 1 ]
 }
 
-export { getFirstByKey, getLastByKey }
+const swapSpread = ( ...sources ) => {
+  return {
+    props: sources.slice( 0, sources.length - 1 ),
+    obj: sources.slice( 0, sources.length )[ sources.length - 1 ],
+  }
+}
+
+const omit = ( ...sources ) => {
+  const { obj, props } = swapSpread( ...sources )
+  return props.reduce( ( p, c ) => {
+    const { [ c ]: omitProp, ...clean } = p
+    return clean
+  }, obj )
+}
+
+export { getFirstByKey, getLastByKey, swapSpread, omit }
