46e513b20c1116dc3ffdf8a8499b22744dac6a3f
nattatorn.dev <nattatorn.dev@gmail.com>
2017-09-06 21:55:28 +0700

    📝 update categories modules

--- modules/Categories/sagas.js
+++ modules/Categories/sagas.js
@@ -1,6 +1,8 @@
 import { all, fork, put, select, take } from 'redux-saga/effects'
 import { SELECT_CHILD_CATEGORY } from './types'
 import { setCurrentCategories } from './actions'
+import { ga } from 'services'
+import config from 'config'
 
 import {
   currentCategoriesSelector,
@@ -15,6 +17,15 @@ function* watchSelectChildCategory() {
       select( currentCategoriesSelector ),
       select( subChildCategoriesNameSelector ),
     ] )
+
+    //TODO: example ga  trackEvent
+    const tracker = new ga.Tracker(
+      config.ga,
+      config.app.name,
+      config.app.version
+    )
+
+    tracker.trackEvent( 'product', 'select product category' )
 
     if ( subChildCategories.length === 0 ) {
       navigation.navigate( 'products', {
