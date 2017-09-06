9950ed24b5a07c44aecddd566d0c14bda86ef19a
nattatorn.dev <nattatorn.dev@gmail.com>
2017-09-06 21:58:42 +0700

    ➕ add datetime utilities

--- utilities/navigation.js
+++ utilities/navigation.js
@@ -7,4 +7,16 @@ const getNavigationParam = ( navigation, param ) => {
   return ''
 }
 
-export { getNavigationParam }
+const getCurrentRouteName = navigationState => {
+  if ( !navigationState ) {
+    return null
+  }
+  const route = navigationState.routes[ navigationState.index ]
+  // dive into nested navigators
+  if ( route.routes ) {
+    return getCurrentRouteName( route )
+  }
+  return route.routeName
+}
+
+export { getNavigationParam, getCurrentRouteName }
