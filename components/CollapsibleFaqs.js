a9a13045fa21fb9e331e362defff82980a280034
nattatorn.dev <nattatorn.dev@gmail.com>
2017-09-11 23:26:19 +0700

    📝 update collapsible shared components

--- components/CollapsibleFaqs.js
+++ components/CollapsibleFaqs.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import values from 'lodash/values'
+import { StyleSheet, Text, View } from 'react-native'
 import Accordion from 'react-native-collapsible/Accordion'
-import { View, Text } from 'react-native'
-import { Colors } from 'constants'
 import { Icon } from 'react-native-elements'
+import PropTypes from 'prop-types'
 
-const styles = {
-  rowTitle: {
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-    borderBottomWidth: 1,
-    borderColor: '#ddd',
-    flexDirection: 'row',
-    minWidth: 12,
-    padding: 20,
+import values from 'lodash/values'
+import { Colors } from 'constants'
+
+const styles = StyleSheet.create( {
+  descriptionText: {
+    fontSize: 13,
+    lineHeight: 20,
+    textAlign: 'justify',
+    color: Colors.textDescription,
   },
   rowDescription: {
     alignItems: 'center',
@@ -23,18 +22,21 @@ const styles = {
     flexDirection: 'row',
     padding: 20,
   },
+  rowTitle: {
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+    flexDirection: 'row',
+    minWidth: 12,
+    padding: 20,
+  },
   titleText: {
     fontSize: 14,
   },
-  descriptionText: {
-    color: Colors.textDescription,
-    fontSize: 13,
-    lineHeight: 20,
-    textAlign:'justify',
-  },
-}
+} )
 
-const AccordionView = ( { faqs } ) => {
+const AccordionView = ( { faqs, onChange } ) => {
   const renderTitle = ( section, index, isActive ) => {
     return (
       <View style={styles.rowTitle}>
@@ -84,14 +86,25 @@ const AccordionView = ( { faqs } ) => {
     )
   }
 
+  const computedFaqs = values( faqs )
+
+  const onCollapsibleChang = index =>
+    !isNaN( parseInt( index ) ) && onChange( computedFaqs[ index ].question )
+
   return (
     <Accordion
-      underlayColor="transparent"
-      sections={values( faqs )}
-      renderHeader={renderTitle}
+      onChange={index => onCollapsibleChang( index )}
       renderContent={renderDescription}
+      renderHeader={renderTitle}
+      sections={computedFaqs}
+      underlayColor="transparent"
     />
   )
+}
+
+AccordionView.propTypes = {
+  faqs: PropTypes.object,
+  onChange: PropTypes.func.isRequired,
 }
 
 export default AccordionView
