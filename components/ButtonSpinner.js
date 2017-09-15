6da8d5a29517ace880ee743c44ce47aae31fb73d
nattatorn.dev <nattatorn.dev@gmail.com>
2017-09-15 22:52:01 +0700

    ➕ ButtonSpinne, SearchHighlight, SearchSpinner on components

---
+++ components/ButtonSpinner.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { View, ActivityIndicator } from 'react-native'
+import PropTypes from 'prop-types'
+
+const styles = {
+  spinnerStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}
+
+const ButtonSpinner = ( { containerStyle, size } ) => {
+  const style = { ...styles.spinnerStyle, ...containerStyle }
+  return (
+    <View style={style}>
+      <ActivityIndicator size={size || 'large'} />
+    </View>
+  )
+}
+
+ButtonSpinner.propTypes = {
+  containerStyle: PropTypes.number.isRequired,
+  size: PropTypes.string.isRequired,
+}
+
+export default ButtonSpinner
