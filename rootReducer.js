4b6ae8fcff1dd2f86a4a43946ccb24ceac162cc4
nattatorn.dev <nattatorn.dev@gmail.com>
2017-09-19 23:59:42 +0700

    ➕ add home reducers

--- rootReducer.js
+++ rootReducer.js
@@ -5,6 +5,7 @@ import { reducers as companyProfileReducer } from 'modules/CompanyProfile'
 import { reducers as contactsReducer } from 'modules/Contacts'
 import { reducers as contactUsReducer } from 'modules/ContactUs'
 import { reducers as faqsReducer } from 'modules/Faqs'
+import { reducers as HomeReducer } from 'modules/Home'
 import { reducers as houseCategoriesReducer } from 'modules/HouseCategories'
 import { reducers as productCategoriesReducer } from 'modules/ProductCategories'
 import { reducers as productsReducer } from 'modules/Products'
@@ -20,6 +21,7 @@ const rootReducer = combineReducers( {
   ...contactsReducer,
   ...contactUsReducer,
   ...faqsReducer,
+  ...HomeReducer,
   ...houseCategoriesReducer,
   ...productCategoriesReducer,
   ...productsReducer,
