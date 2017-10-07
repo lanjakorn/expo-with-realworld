5a90b0664ec43bf3a9c09dc2a6256986f4d1d780
nattatorn.dev <nattatorn.dev@gmail.com>
2017-10-07 22:08:56 +0700

    📝 update gradient background of headerNav

--- components/HeaderNavigation.js
+++ components/HeaderNavigation.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Dimensions, Platform, StyleSheet, Text, View } from 'react-native'
 import { Icon } from 'react-native-elements'
+import { LinearGradient } from 'expo'
 import PropTypes from 'prop-types'
 
 import { Colors } from 'constants'
@@ -16,21 +17,21 @@ const styles = StyleSheet.create( {
   },
   leftSection: {
     alignItems: 'center',
-    backgroundColor: Colors.tintColor,
+    backgroundColor: 'transparent',
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'flex-start',
   },
   centerSection: {
     alignItems: 'center',
-    backgroundColor: Colors.tintColor,
+    backgroundColor: 'transparent',
     flex: 2,
     flexDirection: 'row',
     justifyContent: 'center',
   },
   rightSection: {
     alignItems: 'center',
-    backgroundColor: Colors.tintColor,
+    backgroundColor: 'transparent',
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'flex-end',
@@ -45,47 +46,55 @@ const styles = StyleSheet.create( {
 const HeaderNavigation = ( { navigation, title } ) => {
   return (
     <View style={{ backgroundColor: Colors.tintColor }}>
-      <View style={styles.container}>
-        <View style={styles.leftSection}>
-          <Icon
-            size={32}
-            color={'white'}
-            name={'ios-arrow-back'}
-            type={'ionicon'}
-            underlayColor={Colors.tintColor}
-            onPress={() => {
-              navigation.goBack( null )
-            }}
-            iconStyle={{
-              backgroundColor: Colors.tintColor,
-              textAlign: 'left',
-              width: 50,
-            }}
-          />
+      <LinearGradient
+        colors={[ '#CE1D45', '#E0244F', '#E33B61' ]}
+        start={{ x: 0.0, y: 0.25 }}
+        end={{ x: 1.0, y: 1.0 }}
+      >
+        <View style={styles.container}>
+          <View style={styles.leftSection}>
+            <Icon
+              size={28}
+              color={'white'}
+              name={'ios-arrow-back'}
+              type={'ionicon'}
+              underlayColor={'transparent'}
+              underlineColorAndroid={'transparent'}
+              onPress={() => {
+                navigation.goBack( null )
+              }}
+              iconStyle={{
+                backgroundColor: 'transparent',
+                textAlign: 'left',
+                width: 50,
+              }}
+            />
+          </View>
+          <View style={styles.centerSection}>
+            <Text style={styles.title} numberOfLines={1}>
+              {title}
+            </Text>
+          </View>
+          <View style={styles.rightSection}>
+            <Icon
+              size={35}
+              color={'white'}
+              name={'ios-search'}
+              type={'ionicon'}
+              underlayColor={'transparent'}
+              underlineColorAndroid={'transparent'}
+              onPress={() => {
+                navigation.navigate( 'search', { module: 'search' } )
+              }}
+              iconStyle={{
+                backgroundColor: 'transparent',
+                textAlign: 'right',
+                width: 50,
+              }}
+            />
+          </View>
         </View>
-        <View style={styles.centerSection}>
-          <Text style={styles.title} numberOfLines={1}>
-            {title}
-          </Text>
-        </View>
-        <View style={styles.rightSection}>
-          <Icon
-            size={28}
-            color={'white'}
-            name={'ios-search'}
-            type={'ionicon'}
-            underlayColor={Colors.tintColor}
-            onPress={() => {
-              navigation.navigate( 'search', { module: 'search' } )
-            }}
-            iconStyle={{
-              backgroundColor: Colors.tintColor,
-              textAlign: 'right',
-              width: 50,
-            }}
-          />
-        </View>
-      </View>
+      </LinearGradient>
     </View>
   )
 }
